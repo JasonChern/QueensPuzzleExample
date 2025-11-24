a723002c896c35a4940f104d0ce30e7e80c4eadb
JasonChern <qaz610514@gmail.com>
2025-11-24 21:20:44 +0800

    feat: create  project

---
+++ EntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QueensPuzzleExample
+{
+    internal class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            RunApplication();
+        }
+
+        static void RunApplication()
+        {
+            Console.WriteLine("Hello, World!.");
+        }
+    }
+}
